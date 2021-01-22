6d95527fe57a01dadaa02fe5f53ed7fb090c760e
Kartheek Solipuram <kartheek.s@gmail.com>
2021-01-22 03:39:26 -0800

    Rename package dependency branch names with suffix *-next.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/val-verde/swift-syntax.git", .branch("val-verde-mainline")),
+        .package(url: "https://github.com/val-verde/swift-syntax.git", .branch("val-verde-mainline-next")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
